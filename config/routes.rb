fd70a0e1b60e7d3b272f75bb07c0485aff1fd23c
Hossein Zabolzadeh <hossein@debian.itrc.ac.ir>
2013-06-12 09:53:11 -0400

    Implementing user signup

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ RailsTemplateApp::Application.routes.draw do
   resources :users
 
   root :to => 'static_pages#home'
-  match '/signup', :to => 'users#new' 
+  match '/signup', :to => 'users#new'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
