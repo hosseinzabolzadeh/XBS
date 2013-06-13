6e408aab737bba6ec410ef511c8f6015f003aeb2
hossein <hossein@hossein-HP-ProBook-4520s.(none)>
2013-06-13 14:39:44 +0430

    Finish User Signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
    @user = User.new(params[:user])
    if @user.save
     redirect_to @user
+    flash[:success] = "Welcome to the Sample App!"
    else
     render 'new'
    end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,10 +14,10 @@ class User < ActiveRecord::Base
   attr_accessible :email, :first_name, :last_name, :password, :password_confirmation
   has_secure_password
   before_save { |user| user.email = email.downcase }
-  validates(:first_name, presence: true, length: {maximum: 50})
-  validates(:last_name, presence: true, length: {maximum: 50})
+  validates(:first_name, :presence => true, :length => {:maximum => 50})
+  validates(:last_name, :presence => true, :length => {:maximum => 50})
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates(:email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false } )
-  validate(:password, presence: true, length: {minimum: 6 } )
-  validate(:password_confirmation, presence: true)
+  validates(:email, :presence => true, :format => { :with => VALID_EMAIL_REGEX }, :uniqueness => { :case_sensitive => false } )
+  validate(:password, :presence => true, :length => {:minimum => 6 } )
+  validate(:password_confirmation, :presence => true)
 end
