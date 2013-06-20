a41a8bcfb3304cb59acc2dc5cc2be60a8ace0b86
hossein <hossein@hossein-HP-ProBook-4520s.(none)>
2013-06-20 09:54:29 +0430

    Impoving user profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :first_name, :last_name, :password, :password_confirmation
+  attr_accessible :email, :first_name, :last_name, :password, :password_confirmation, :testa
   has_secure_password
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
